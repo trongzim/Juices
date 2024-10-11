30d359110777e808572de8077a5a474153e52691
trongzim <trongzim123@gmail.com>
2024-10-11 11:41:11 +0700

    Cập nhật dự án

--- routes/route.js
+++ routes/route.js
@@ -1,30 +1,44 @@
-    const express = require('express');
-    const router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
-    const fs = require('fs');
-    const path = require('path');
-
-    router.get('/products/:id', (req, res) => {
-        const filePath = path.join(__dirname, 'data/products.json');
-        const productId = req.params.id;
-    
-        fs.readFile(filePath, 'utf-8', (err, data) => {
-            if (err) {
-                return res.status(500).send('Không thể đọc tệp JSON.');
-            }
-            try {
-                const products = JSON.parse(data);
-                const product = products.find(p => p.id === productId);
-                if (!product) {
-                    return res.status(404).send('Sản phẩm không tồn tại.');
-                }
-                res.status(200).json(product);
-            } catch (parseErr) {
-                res.status(500).send('Lỗi trong quá trình parse tệp JSON.');
-            }
-        });
+// Lấy danh sách tất cả các sản phẩm
+router.get('/products', (req, res) => {
+    const filePath = path.join(__dirname, '../data/products.json');
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Không thể đọc tệp JSON.');
+        }
+        try {
+            const products = JSON.parse(data);
+            res.status(200).json(products);
+        } catch (parseErr) {
+            res.status(500).send('Lỗi trong quá trình parse tệp JSON.');
+        }
     });
-   
+});
 
+// Lấy thông tin chi tiết sản phẩm theo ID
+router.get('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const filePath = path.join(__dirname, '../data/products.json');
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Không thể đọc tệp JSON.');
+        }
+        try {
+            const products = JSON.parse(data);
+            const product = products.find(p => p.id == productId);
+            if (product) {
+                res.status(200).json(product);
+            } else {
+                res.status(404).send('Không tìm thấy sản phẩm.');
+            }
+        } catch (parseErr) {
+            res.status(500).send('Lỗi trong quá trình parse tệp JSON.');
+        }
+    });
+});
 
-    module.exports = router;
+module.exports = router;
